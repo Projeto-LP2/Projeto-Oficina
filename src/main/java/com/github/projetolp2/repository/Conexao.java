42e343bf84f063fd9a771fcdf1668fd4f9d2016b
Ricardo <65051149+RicardoR211@users.noreply.github.com>
2026-08-26 22:46:03 -0300

    Modelo do Projeto

--- src/main/java/com/github/projetolp2/repository/Conexao.java
+++ src/main/java/com/github/projetolp2/repository/Conexao.java
@@ -1,15 +1,52 @@
 package com.github.projetolp2.repository;
 
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Conexao {
 
-    private static final String DRIVE_MYSQL = "org.postgresql.Driver";
-    private static final String ENDERECO = "jdbc:postgresql://localhost:5432/oficina";
-    private static final String USUARIO = "postgres";
-    private static final String SENHA = "12345";
+    private static String driver;
+    private static String endereco;
+    private static String usuario;
+    private static String senha;
 
-    //public static Connection getConexao() {}
+    static{
+        try (InputStream input = Conexao.class.getClassLoader()
+                .getResourceAsStream("application.properties")){
 
-    //public static void fecharConexao(Connection con) {}
+            Properties prop = new Properties();
+            prop.load(input);
+
+            driver = prop.getProperty("db.driver");
+            endereco = prop.getProperty("db.endereco");
+            usuario = prop.getProperty("db.usuario");
+            senha = prop.getProperty("db.senha");
+
+        } catch (Exception e) {
+            System.err.println("Erro ao carregar application.properties: " + e.getMessage());
+        }
+    }
+    public static Connection getConexao() {
+        Connection con = null;
+
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(endereco, usuario, senha);
+        } catch (ClassNotFoundException e) {
+            System.err.println("Driver não encontrado: " + e.getMessage());
+        } catch (SQLException e) {
+            System.err.println("Erro ao conectar: " + e.getMessage());
+        }
+
+        return con;
+    }
+
+    public static void fecharConexao(Connection con) {
+
+    }
 
     //public static void fecharConexao(Connection con, PreparedStatement stmt) {}
 
